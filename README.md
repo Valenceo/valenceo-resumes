8139d475db830543e06692930360493fe479cff0
Matt Hayden (Valenceo LTD.) <https://github.com/Valenceo>
2017-09-13 04:15:02 +0000

    More doc

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# valenceo-resumes
-valenceo-resumes
+# Resume builder for contractors of Valenceo, LTD.
+
+This is for public-facing resumes related to the consulting firm Valenceo LTD.
+Feel free to re-use the machinery to post your own resume(s). I stand on the
+shoulders of others, like hackmyresume and the Kendall theme.
+
+Installation:
+
+1. npm install
+1. ./refresh.bash
+
+Suggested system packages for Google Cloud buckets:
+
+1. jq
+
+Suggested packages for PDF output:
+
+1. pandoc,texlive-latex-recommended
+1. wkhtmltopdf or phantomjs
+
+TODO:
+
+1. DOC, DOCX output are lacking.
+1. hackmyresume doesn't like PDF arguments, or I'm mistreating it
--- refresh.bash
+++ refresh.bash
@@ -5,14 +5,36 @@ function acl_is_world_readable() {
   jq -e '.[]? | select(.entity=="allUsers") | select(.role=="READER") | any'
 }
 
-source refresh.config
+source config
 for name in $names
 do
+  dest=${site}
   theme=node_modules/jsonresume-theme-kendall
-  hackmyresume BUILD jrs/${name}.json TO ${site}/${name}/index.html --theme ${theme}
+  if hackmyresume BUILD jrs/${name}.json TO ${dest}/${name}/index.html --theme ${theme}
+  then
+    echo HTML
+  fi
   theme=node_modules/jsonresume-theme-onepage
-  hackmyresume BUILD jrs/${name}.json TO ${site}/${name}/print/index.html --theme ${theme}
+  if hackmyresume BUILD jrs/${name}.json TO ${dest}/${name}/print/index.html --theme ${theme}
+  then
+    echo 'PRINT (html)'
+  fi
+  if pandoc ${dest}/${name}/print/index.html -o ${dest}/${name}/${name}.doc
+  then
+    echo DOC
+  fi
+  if hackmyresume BUILD jrs/${name}.json TO ${dest}/${name}/${name}.pdf -p wkhtmltopdf --theme ${theme}
+  then
+    echo PDF
+  fi
 done
+
+read -p "Push to server [YyNn]> "
+case $REPLY in
+	Y|y) echo Pushing to ${site} ;;
+	N|n) exit 0 ;;
+	*) echo Not pushing to ${site} ;;
+esac
 
 if gsutil defacl get gs://resumes.valenceo.com | acl_is_world_readable
 then
@@ -21,4 +43,5 @@ else
   echo gsutil acl ch -r -u AllUsers:R gs://${site}
 fi
 
-gsutil rsync -r ${site} gs://${site}
+src=${site}
+gsutil rsync -r ${src} gs://${site}
